01267291a2b6db8235691e08ea72898951a52ed6
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2025-06-18 19:08:26 -0400

    started adding util class for Path

---
+++ src/main/java/edu/s3rl/utils/PathUtils.java
@@ -0,0 +1,86 @@
+package edu.s3rl.utils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * Utility class for common file operations.
+ */
+public class PathUtils {
+
+    /**
+     * Private constructor is to prevent the instantiation of this class directly.
+     * Throws {@link UnsupportedOperationException} to prevent instantiation,
+     * including attempts via reflection.
+     */
+    private PathUtils() {
+        throw new UnsupportedOperationException("This class cannot be instantiated");
+    }
+
+    /**
+     * Checks whether the specified file or folder exists in the file system
+     *
+     * @param path the {@link Path} to the file or folder; must not be {@code null}
+     * @return {@code true} if the path exists; {@code false} otherwise
+     * @throws IllegalArgumentException if {@code path} is {@code null}
+     */
+    public static boolean exists(Path path) {
+
+        if (path == null) {
+            throw new IllegalArgumentException("path should not be null");
+        }
+
+        return Files.exists(path);
+    }
+
+    public static void writeToFile(Path file, String content) throws IOException {
+
+        writeToFile(file, content, false);
+    }
+
+    public static void writeToFile(Path file, String content, boolean append) throws IOException {
+
+        if (file == null) {
+            throw new IllegalArgumentException("File should not be null");
+        }
+
+        if (content == null) {
+            throw new IllegalArgumentException("Content should not be null");
+        }
+
+        if (isFolder(file)) {
+            throw new IOException("File exists but it is a directory. Expected a regular file.");
+        }
+
+        if (append) {
+            Files.writeString(file, content, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        } else {
+            Files.writeString(file, content, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        }
+    }
+
+    /**
+     * Returns the path to the system temporary folder. This is determined
+     * by the {@code java.io.tmpdir} system property.
+     *
+     * @return the {@link Path} representing the system temporary directory
+     */
+    public static Path tempFolder() {
+
+        String tempDir = System.getProperty("java.io.tmpdir");
+
+        return Path.of(tempDir);
+    }
+
+    public static boolean isFile(Path file) {
+        return Files.isRegularFile(file);
+    }
+
+    public static boolean isFolder(Path file) {
+        return Files.isDirectory(file);
+    }
+
+}
---
+++ src/test/java/test/edu/s3rl/utils/PathUtilsTest.java
@@ -0,0 +1,76 @@
+package test.edu.s3rl.utils;
+
+import edu.s3rl.utils.PathUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PathUtilsTest {
+
+    @Test
+    void testIfConstructorIsInstantiatedThenThrowException() {
+
+        assertThrows(InvocationTargetException.class, () -> {
+
+            Constructor<PathUtils> ctor = PathUtils.class.getDeclaredConstructor();
+
+            // Since the constructor is private, you need to make it
+            // accessible to invocation
+            ctor.setAccessible(true);
+
+            ctor.newInstance();
+        });
+    }
+
+    @Test
+    void testExistsIfPathIsNullThenThrowException() {
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            PathUtils.exists(null);
+        });
+    }
+
+    @Test
+    void testExistsIfFileExistsThenReturnTrue() {
+
+        Path actual = Path.of("pom.xml");
+
+        assertTrue(PathUtils.exists(actual));
+    }
+
+    @Test
+    void testExistsIfFolderExistsThenReturnTrue() {
+
+        Path actual = Path.of("src");
+
+        assertTrue(PathUtils.exists(actual));
+    }
+
+    @Test
+    void testTempFolderShouldNotReturnNull() {
+
+        assertNotNull(PathUtils.tempFolder());
+    }
+
+    @Test
+    void test() {
+
+        String content = "Just a Test";
+        Path tmpFolder = PathUtils.tempFolder().resolve("test.txt");
+
+        try {
+            PathUtils.writeToFile(tmpFolder, content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertTrue(true);
+//        assertNotNull(FileUtils.tempFolder());
+    }
+
+}
